c217b00bfaf2d3c6634f28a832d4c159cb1b4a64
Al-Mahdi Utshob <mahdiutshob@gmail.com>
2023-06-01 11:59:46 +0600

    Stater Kit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.master')
+
+@section('title', 'Default')
+
+@section('css')
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/animate.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/prism.css') }}">
+@endsection
+
+@section('style')
+@endsection
+
+@section('breadcrumb-title')
+    <h3>Default</h3>
+@endsection
+
+@section('breadcrumb-items')
+    <li class="breadcrumb-item">Dashboard</li>
+    <li class="breadcrumb-item active">Default</li>
+@endsection
+
+@section('content')
+
+@endsection
+
+@section('script')
+@endsection
